277504ec8eee059903a91578a1b4c03f52867bf4
SashaCursos <152596934+SashaCursos@users.noreply.github.com>
2024-01-22 13:35:09 +0100

    Update config.py
    civit to hugging

--- modules/config.py
+++ modules/config.py
@@ -258,7 +258,8 @@ default_image_number = get_config_item_or_set_default(
 checkpoint_downloads = get_config_item_or_set_default(
     key='checkpoint_downloads',
     default_value={
-    "juggernautXL_version8Rundiffusion.safetensors": "https://civitai.com/api/download/models/288982?type=Model&format=SafeTensor&size=full&fp=fp16"
+    "juggernautXL_version8Rundiffusion.safetensors": "https://huggingface.co/SashaCursos/fav_models/resolve/main/fav/juggernautXL_v8Rundiffusion.safetensors?download=true"    
+    #"juggernautXL_version8Rundiffusion.safetensors": "https://civitai.com/api/download/models/288982?type=Model&format=SafeTensor&size=full&fp=fp16"
     #"juggernautXL_version6Rundiffusion.safetensors": "https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/juggernautXL_version6Rundiffusion.safetensors"
     },
     validator=lambda x: isinstance(x, dict) and all(isinstance(k, str) and isinstance(v, str) for k, v in x.items())
