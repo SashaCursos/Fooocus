44dc165043ec3ce1ca6dd5a14ab0bed4738b707d
SashaCursos <152596934+SashaCursos@users.noreply.github.com>
2024-01-24 08:58:11 +0100

    Actualizar config.py

--- modules/config.py
+++ modules/config.py
@@ -155,7 +155,9 @@ def get_config_item_or_set_default(key, default_value, validator, disable_empty_
 default_base_model_name = get_config_item_or_set_default(
     key='default_model',
     #default_value='juggernautXL_version6Rundiffusion.safetensors',
-    default_value='juggernautXL_version8Rundiffusion.safetensors',
+    #default_value='juggernautXL_version8Rundiffusion.safetensors',
+    default_value='SDXXXL.safetensors',
+    #default_value='DreamShaperXL.safetensors',
     validator=lambda x: isinstance(x, str)
 )
 default_refiner_model_name = get_config_item_or_set_default(
